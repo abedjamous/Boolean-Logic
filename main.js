0d3bfa9caf490475d7ae09b5d29589c3a926d594
abed jamous <jamous.hameed94@gmail.com>
2020-07-16 07:05:03 -0400

    if and else statment

---
+++ main.js
@@ -0,0 +1,25 @@
+// Boolean logic 
+let firstame = "Jhon";
+let Jhonage = 18;
+
+if(Jhonage > 18) {
+    console.log(firstame + " is a man");
+} else {
+    console.log(firstame + " is a teenager");
+};
+
+firstame = "Mark";
+let age = 20;
+
+if(age < 13) {
+    console.log(firstame + " is a boy");
+} else if(age >= 13 && age < 20) {
+    console.log(firstame + " is a teenager");
+} else if(age >= 20 && age < 30) {
+    console.log(firstame + " is a young man");
+} else {
+    console.log(firstame + " is a man");
+};
+
+
+
